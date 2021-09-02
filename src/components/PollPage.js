1e3339febd67131c7991e6c741c7ded76b41e907
Shafei-AOC <shafei-aoc@pop-os.localdomain>
2021-09-02 13:24:56 +0200

    newly created and non-existence polls redirected to the 404 route fixed

--- src/components/PollPage.js
+++ src/components/PollPage.js
@@ -1,11 +1,23 @@
-import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useHistory, useParams } from "react-router-dom";
 import Poll from "./Poll";
 
 function PollPage() {
     const { QuestionId } = useParams()
+    const {polls} = useSelector(state => state )
+    const history = useHistory()
+    console.log(!polls[QuestionId])
+    if (!polls[QuestionId]) {
+        console.log("404")
+        history.push("/404")
+    }
+
     return (
         <div className="max-w-2xl h-96 pt-12 mx-auto my-auto rounded-lg">
-            <Poll pollId={QuestionId} pollPage={true}/>
+            {polls[QuestionId] &&  
+                <Poll pollId={QuestionId} pollPage={true}/>
+            }
         </div>
     )
 }
